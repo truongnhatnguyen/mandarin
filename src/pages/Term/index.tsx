2597b4a2c2b409f2f1f203bbd06466b001db72e3
truongnhatnguyen <truongnhatnguyen9999@gmail.com>
2023-05-10 09:13:48 +0700

    feature/term conditions page

--- src/pages/Term/index.tsx
+++ src/pages/Term/index.tsx
@@ -1,109 +1,116 @@
-import { LN, P, T } from "../../shared/components/Docs";
+import { NavLink } from "react-router-dom";
 
 export function TermPage() {
- return (
-  <div className="container-auto py-12">
-   <T>Terms and Conditions</T>
-   <P>
-    These Terms and Conditions, together with any and all other documents
-    referred to herein, set forth the terms of use under which you may use this
-    website, https://atus.network/ ("Website). Please read these Terms and
-    Conditions carefully and make sure you understand them. Your agreement to
-    comply with and be bound by these Terms and Conditions is deemed to have
-    occurred when you first used the Website. If you do not agree to comply with
-    and be bound by these Terms and Conditions, you must stop using this Website
-    immediately.
-   </P>
-   <T>Definition and Explanation</T>
-   <P>
-    In these Terms and Conditions, unless the context otherwise requires, the
-    following expressions have the following meanings:
-   </P>
-   <P>"Content"</P>
-   means any and all text, images, audio, video, scripts, code, software,
-   databases and any other form of information potentially stored on a computer{" "}
-   <P>appearing on or forming part of the Website;</P>
-   <P>“We/We/Our”</P>
-   <P>means Atus, a company registered in the United States.</P>
-   <T>Access to the Websit</T>
-   <LN>
-    <li>Access to the website is free.</li>
-    <li>
-     Access to the Website is provided “as is” and on an “as available” basis.
-     We may change, suspend or discontinue the provision of the Website (or any
-     part thereof) at any time without prior notice. We will not be liable to
-     you in any way if the Website (or any part of it) becomes unavailable at
-     any time and for any period of time.
-    </li>
-   </LN>
-   <T>Links to Other Websites</T>
-   <P>
-    Links to other websites may be included on the Website. External links to
-    other websites contained on this Website are provided for convenience and
-    for informational purposes only.
-   </P>
-   <P>
-    Unless expressly stated, these other websites are not under Our control. We
-    neither acknowledge nor accept responsibility or liability for the content
-    of third party websites. The inclusion of a link to another web page on the
-    Website is for informational purposes only.
-   </P>
-   <T>Limitation of Liability</T>
-   <P>
-    In no event shall the mutual liability of the Protected Parties arise out of
-    or in connection with Atus's Website, Services and Network (and any content
-    and functionality thereof), any activities or inactivity of the Services,
-    your Distributed Ledger Tech assets, other activities digital assets, ATU
-    Tokens or any other product, service or item offered by or on behalf of the
-    Protected Party, whether under contract, tort (including negligence), civil
-    liability, statute, strict liability or other liability theory in addition
-    to the fees you must have paid to us under these Terms during the twelve
-    (12) month period immediately preceding the event giving rise to the
-    liability claim.
-   </P>
-   <T>Disclaimers</T>
-   <P>
-    We make no representations, warranties or guarantees that the Website will
-    meet your requirements, that the Website will not infringe the rights of
-    third parties, that the Website will be compatible with all software and
-    hardware.
-   </P>
-   <P>
-    The content on this Website does not constitute advice that you should rely
-    on. It is provided for general information purposes only. It is always
-    advisable to seek expert or expert advice before taking any action in
-    connection with any product or service mentioned or linked to on this
-    Website.
-   </P>
-   <P>
-    We are not responsible for the accuracy, legality or content of external
-    websites or subsequent links. Contact the external website for answers to
-    questions regarding its content.
-   </P>
-   <P>
-    We make reasonable efforts to ensure that the Content on this Website is
-    complete, accurate and up to date. However, we do not make any
-    representations, warranties or guarantees (whether express or implied) that
-    the content is complete, accurate or up to date.
-   </P>
-   <T> Privacy and Cookies</T>
-   <P>
-    Use of our Website is also governed by our Cookies and Privacy Policy,
-    available from our Cookie Policy and Privacy Policy. These policies are
-    incorporated into these Terms and Conditions by reference.
-   </P>
-   <T> Changes to these Terms and Conditions</T>
-   <P>
-    We may change these Terms and Conditions at any time. Any such changes will
-    be binding on you when you use the Website for the first time after the
-    changes have been implemented. Therefore, you should check this page from
-    time to time.
-   </P>
-   <P>
-    In the event of any conflict between the current version of these Terms and
-    Conditions and any prior version(s), the current and effective terms shall
-    prevail unless otherwise explicitly stated.
-   </P>
-  </div>
- );
+  return (
+    <>
+      <div>
+        <div className="cs-height_90 cs-height_lg_80" />
+        {/* Start Page Head */}
+        <section
+          className="cs-page_head cs-bg"
+          data-src="assets/img/page_head_bg.svg"
+        >
+          <div className="container-auto">
+            <div className="text-center">
+              <h1 className="cs-page_title">Terms &amp; Conditions</h1>
+              <ol className="breadcrumb">
+                <li className="breadcrumb-item">
+                  <NavLink to="/">Home</NavLink>
+                </li>
+                <li className="breadcrumb-item active">
+                  Terms &amp; Conditions
+                </li>
+              </ol>
+            </div>
+          </div>
+        </section>
+        {/* End Page Head */}
+        <div className="cs-height_100 cs-height_lg_70" />
+        <div className="container-auto">
+          <div className="row">
+            <div className="col-lg-8 offset-lg-2">
+              <div className="cs-single_post">
+                <h3>Terms &amp; Conditions</h3>
+                <p>
+                  “Account” long established fact that a reader will be distrac
+                  by the readable content of a page when looking at its layout.
+                  The point of using Lorem Ipm i has a more-ornormal
+                  distribution of letters,as opposed It is a long established
+                  fact that a reader will be distrac by the readable content of
+                  a page when looking at itslayout. The point of using Lorem Ipm
+                  i has a NFTs
+                </p>
+                <ul>
+                  <li>
+                    Crypto is a long established fact that a reader will be
+                    distrac by the readable content of a page when looking at
+                    its layout. The point of using Lorem Ipm i has a
+                    more-ornormal{" "}
+                  </li>
+                  <li>
+                    letters,as opposed It is a long established fact that a
+                    reader will be distrac by the readable content of a page
+                    when looking at itslayout. The point of using Lorem Ipm i
+                    has a NFTs
+                  </li>
+                </ul>
+                <h3>Fees and Payment.</h3>
+                <p>
+                  “NFTS Music” is a long established fact that a reader will be
+                  distrac by the readable content of a page when looking at its
+                  layout. The point of using Lorem Ipm i has a more-ornormal
+                  distributio. NFT Arts as opposed It is a long established fact
+                  that a reader will be distrac by the readable content of a
+                  page when looking at itslayout. The point of using Lorem Ipm i
+                  has a NFTs
+                </p>
+                <ul>
+                  <li>How to make and sell an NFT.</li>
+                  <li>Buy some cryptocurrency to fund your wallet.</li>
+                  <li>Add some cryptocurrency to your wallet.</li>
+                  <li>Upload the file you want to turn into an NFT.</li>
+                  <li>Set up an auction for your NFT</li>
+                </ul>
+                <h3>Temporary Suspension; Limiting API Requests</h3>
+                <p>
+                  NFTs a long established fact that a reader will be distrac by
+                  the readable content of a page when looking at its layout. The
+                  point of using Lorem Ipm i has a.
+                </p>
+                <h3>Accessing the Service</h3>
+                <p>
+                  NFTs a long established fact that a reader will be distrac by
+                  the readable content of a page when looking at its layout. The
+                  point of using Lorem Ipm i has a. <br /> NFTs a long
+                  established fact that a reader will be distrac by the readable
+                  content of a page when looking at its layout. The point of
+                  using Lorem Ipm i has a.
+                </p>
+                <h3>License to Access and Use Our Service and Content</h3>
+                <p>
+                  NFTs a long established fact that a reader will be distrac by
+                  the readable content of a page when looking at its layout. The
+                  point of using Lorem Ipm i has a.
+                </p>
+                <ul>
+                  <li>
+                    Use or attempt to use another user’s Account without
+                    authorization from such user
+                  </li>
+                  <li>Pose as another person or entity</li>
+                </ul>
+                <h3>Miscellaneous</h3>
+                <p>
+                  NFTs a long established fact that a reader will be distrac by
+                  the readable content of a page when looking at its layout. The
+                  point of using Lorem Ipm i has a.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="cs-height_60 cs-height_lg_30" />
+      </div>
+    </>
+  );
 }
