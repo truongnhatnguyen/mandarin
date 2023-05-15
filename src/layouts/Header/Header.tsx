3b5a1984ccdb9658a11353c75c42774c8d127a8c
PhuongHieuHo <fuhi.dev@gmail.com>
2023-05-15 21:46:56 +0700

    Bổ sung Connect Wallet

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -2,44 +2,44 @@
 import { NavLink } from "react-router-dom";
 import { Menu } from "../Menu";
 export const Header = () => {
-  return (
-    <header className="cs-site_header cs-style1 cs-sticky-header cs-white_bg">
-      <div className="cs-main_header">
-        <div className="container-fluid">
-          <div className="cs-main_header_in">
-            <div className="cs-main_header_left">
-              <NavLink className="cs-site_branding" to="index.html">
-                <img src="assets/home/logo.png" alt="Logo" />
-              </NavLink>
-            </div>
-            <div className="cs-main_header_right">
-              <Menu />
-              <div className="cs-header_btns_wrap">
-                <div className="cs-header_btns">
-                  <div className="cs-header_icon_btn cs-center cs-mobile_search_toggle">
-                    <svg
-                      width={20}
-                      height={21}
-                      viewBox="0 0 20 21"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M9.16667 16.3333C12.8486 16.3333 15.8333 13.3486 15.8333 9.66667C15.8333 5.98477 12.8486 3 9.16667 3C5.48477 3 2.5 5.98477 2.5 9.66667C2.5 13.3486 5.48477 16.3333 9.16667 16.3333Z"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                      <path
-                        d="M17.5 18L13.875 14.375"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                    </svg>
-                  </div>
+ return (
+  <header className="cs-site_header cs-style1 cs-sticky-header cs-white_bg">
+   <div className="cs-main_header">
+    <div className="container-fluid">
+     <div className="cs-main_header_in">
+      <div className="cs-main_header_left">
+       <NavLink className="cs-site_branding" to="/">
+        <img src="assets/home/logo.png" alt="Logo" />
+       </NavLink>
+      </div>
+      <div className="cs-main_header_right">
+       <Menu />
+       <div className="cs-header_btns_wrap">
+        <div className="cs-header_btns">
+         <div className="cs-header_icon_btn cs-center cs-mobile_search_toggle">
+          <svg
+           width={20}
+           height={21}
+           viewBox="0 0 20 21"
+           fill="none"
+           xmlns="http://www.w3.org/2000/svg"
+          >
+           <path
+            d="M9.16667 16.3333C12.8486 16.3333 15.8333 13.3486 15.8333 9.66667C15.8333 5.98477 12.8486 3 9.16667 3C5.48477 3 2.5 5.98477 2.5 9.66667C2.5 13.3486 5.48477 16.3333 9.16667 16.3333Z"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+           />
+           <path
+            d="M17.5 18L13.875 14.375"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+           />
+          </svg>
+         </div>
 
-                  {/* <div className="cs-toggle_box cs-profile_box">
+         {/* <div className="cs-toggle_box cs-profile_box">
                     <div className="cs-toggle_btn cs-header_icon_btn cs-center">
                       <svg
                         width={19}
@@ -131,18 +131,15 @@ export const Header = () => {
                       </div>
                     </div>
                   </div> */}
-                  <NavLink
-                    to="connect-wallet.html"
-                    className="cs-btn cs-style1"
-                  >
-                    <span>Connect Wallet</span>
-                  </NavLink>
-                </div>
-              </div>
-            </div>
-          </div>
+         <NavLink to="connect-wallet" className="cs-btn cs-style1">
+          <span>Connect Wallet</span>
+         </NavLink>
         </div>
+       </div>
       </div>
-    </header>
-  );
+     </div>
+    </div>
+   </div>
+  </header>
+ );
 };
--- src/pages/ConnectWallet/index.tsx
+++ src/pages/ConnectWallet/index.tsx
@@ -1,115 +1,114 @@
-import React from "react";
 import { NavLink } from "react-router-dom";
 
 export function ConnectWallet() {
-  return (
-    <>
-      <div>
-        <div className="cs-height_100 cs-height_lg_70" />
-        <div className="container-auto">
-          <div className="cs-section_heading cs-style4">
-            <h2 className="cs-section_title">Connect Your Wallet</h2>
-            <p className="cs-section_subtitle">
-              Amet minim mollit non deserunt ullamco est sit aliqua dolor do{" "}
-              <br /> amet sint. Velit officia consequat duis enim velit mollit.
-            </p>
-          </div>
-          <div className="cs-height_45 cs-height_lg_45" />
-          <div className="grid sm:grid-cols-3 grid-cols-1 gap-x-4">
-            <div className="">
-              <div className="cs-iconbox cs-style3 cs-box_shadow cs-white_bg">
-                <div className="cs-iconbox_img">
-                  <img
-                    src="../assets/img/general/iconbox_logo_1.svg"
-                    alt="Logo"
-                  />
-                </div>
-                <div className="cs-iconbox_text">
-                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-                  amet sint. Velit officia consequat duis enim velit mollit.
-                </div>
-                <NavLink to="#" className="cs-iconbox_btn cs-primary_font">
-                  Connect Wallet
-                  <svg
-                    width={17}
-                    height={12}
-                    viewBox="0 0 17 12"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M16.5303 6.75396C16.8232 6.46107 16.8232 5.9862 16.5303 5.6933L11.7574 0.920332C11.4645 0.627439 10.9896 0.627439 10.6967 0.920332C10.4038 1.21323 10.4038 1.6881 10.6967 1.98099L14.9393 6.22363L10.6967 10.4663C10.4038 10.7592 10.4038 11.234 10.6967 11.5269C10.9896 11.8198 11.4645 11.8198 11.7574 11.5269L16.5303 6.75396ZM0 6.97363H16V5.47363H0V6.97363Z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                </NavLink>
-              </div>
-              <div className="cs-height_30 cs-height_lg_30" />
-            </div>
-            <div className="">
-              <div className="cs-iconbox cs-style3 cs-box_shadow cs-white_bg">
-                <div className="cs-iconbox_img">
-                  <img
-                    src="../assets/img/general/iconbox_logo_2.svg"
-                    alt="Logo"
-                  />
-                </div>
-                <div className="cs-iconbox_text">
-                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-                  amet sint. Velit officia consequat duis enim velit mollit.
-                </div>
-                <NavLink to="#" className="cs-iconbox_btn cs-primary_font">
-                  Connect Wallet
-                  <svg
-                    width={17}
-                    height={12}
-                    viewBox="0 0 17 12"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M16.5303 6.75396C16.8232 6.46107 16.8232 5.9862 16.5303 5.6933L11.7574 0.920332C11.4645 0.627439 10.9896 0.627439 10.6967 0.920332C10.4038 1.21323 10.4038 1.6881 10.6967 1.98099L14.9393 6.22363L10.6967 10.4663C10.4038 10.7592 10.4038 11.234 10.6967 11.5269C10.9896 11.8198 11.4645 11.8198 11.7574 11.5269L16.5303 6.75396ZM0 6.97363H16V5.47363H0V6.97363Z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                </NavLink>
-              </div>
-              <div className="cs-height_30 cs-height_lg_30" />
-            </div>
-            <div className="">
-              <div className="cs-iconbox cs-style3 cs-box_shadow cs-white_bg">
-                <div className="cs-iconbox_img">
-                  <img
-                    src="../assets/img/general/iconbox_logo_3.svg"
-                    alt="Logo"
-                  />
-                </div>
-                <div className="cs-iconbox_text">
-                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-                  amet sint. Velit officia consequat duis enim velit mollit.
-                </div>
-                <NavLink to="#" className="cs-iconbox_btn cs-primary_font">
-                  Connect Wallet
-                  <svg
-                    width={17}
-                    height={12}
-                    viewBox="0 0 17 12"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M16.5303 6.75396C16.8232 6.46107 16.8232 5.9862 16.5303 5.6933L11.7574 0.920332C11.4645 0.627439 10.9896 0.627439 10.6967 0.920332C10.4038 1.21323 10.4038 1.6881 10.6967 1.98099L14.9393 6.22363L10.6967 10.4663C10.4038 10.7592 10.4038 11.234 10.6967 11.5269C10.9896 11.8198 11.4645 11.8198 11.7574 11.5269L16.5303 6.75396ZM0 6.97363H16V5.47363H0V6.97363Z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                </NavLink>
-              </div>
-              <div className="cs-height_30 cs-height_lg_30" />
-            </div>
-          </div>
+ return (
+  <>
+   <div>
+    <div className="cs-height_100 cs-height_lg_70" />
+    <div className="container-auto">
+     <div className="cs-section_heading cs-style4">
+      <h2 className="cs-section_title">Connect Your Wallet</h2>
+      <p className="cs-section_subtitle">
+       Amet minim mollit non deserunt ullamco est sit aliqua dolor do <br />{" "}
+       amet sint. Velit officia consequat duis enim velit mollit.
+      </p>
+     </div>
+     <div className="cs-height_45 cs-height_lg_45" />
+     <div className="grid sm:grid-cols-3 grid-cols-1 gap-x-4">
+      <div className="">
+       <div className="cs-iconbox cs-style3 cs-box_shadow cs-white_bg">
+        <div className="cs-iconbox_img">
+         <img src="../assets/img/general/iconbox_logo_1.svg" alt="Logo" />
         </div>
-        <div className="cs-height_70 cs-height_lg_40" />
+        <div className="cs-iconbox_text">
+         Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+         sint. Velit officia consequat duis enim velit mollit.
+        </div>
+        <NavLink
+         to="/connect-wallet"
+         className="cs-iconbox_btn cs-primary_font"
+        >
+         Connect Wallet
+         <svg
+          width={17}
+          height={12}
+          viewBox="0 0 17 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+         >
+          <path
+           d="M16.5303 6.75396C16.8232 6.46107 16.8232 5.9862 16.5303 5.6933L11.7574 0.920332C11.4645 0.627439 10.9896 0.627439 10.6967 0.920332C10.4038 1.21323 10.4038 1.6881 10.6967 1.98099L14.9393 6.22363L10.6967 10.4663C10.4038 10.7592 10.4038 11.234 10.6967 11.5269C10.9896 11.8198 11.4645 11.8198 11.7574 11.5269L16.5303 6.75396ZM0 6.97363H16V5.47363H0V6.97363Z"
+           fill="currentColor"
+          />
+         </svg>
+        </NavLink>
+       </div>
+       <div className="cs-height_30 cs-height_lg_30" />
       </div>
-    </>
-  );
+      <div className="">
+       <div className="cs-iconbox cs-style3 cs-box_shadow cs-white_bg">
+        <div className="cs-iconbox_img">
+         <img src="../assets/img/general/iconbox_logo_2.svg" alt="Logo" />
+        </div>
+        <div className="cs-iconbox_text">
+         Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+         sint. Velit officia consequat duis enim velit mollit.
+        </div>
+        <NavLink
+         to="/connect-wallet"
+         className="cs-iconbox_btn cs-primary_font"
+        >
+         Connect Wallet
+         <svg
+          width={17}
+          height={12}
+          viewBox="0 0 17 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+         >
+          <path
+           d="M16.5303 6.75396C16.8232 6.46107 16.8232 5.9862 16.5303 5.6933L11.7574 0.920332C11.4645 0.627439 10.9896 0.627439 10.6967 0.920332C10.4038 1.21323 10.4038 1.6881 10.6967 1.98099L14.9393 6.22363L10.6967 10.4663C10.4038 10.7592 10.4038 11.234 10.6967 11.5269C10.9896 11.8198 11.4645 11.8198 11.7574 11.5269L16.5303 6.75396ZM0 6.97363H16V5.47363H0V6.97363Z"
+           fill="currentColor"
+          />
+         </svg>
+        </NavLink>
+       </div>
+       <div className="cs-height_30 cs-height_lg_30" />
+      </div>
+      <div className="">
+       <div className="cs-iconbox cs-style3 cs-box_shadow cs-white_bg">
+        <div className="cs-iconbox_img">
+         <img src="../assets/img/general/iconbox_logo_3.svg" alt="Logo" />
+        </div>
+        <div className="cs-iconbox_text">
+         Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+         sint. Velit officia consequat duis enim velit mollit.
+        </div>
+        <NavLink
+         to="/connect-wallet"
+         className="cs-iconbox_btn cs-primary_font"
+        >
+         Connect Wallet
+         <svg
+          width={17}
+          height={12}
+          viewBox="0 0 17 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+         >
+          <path
+           d="M16.5303 6.75396C16.8232 6.46107 16.8232 5.9862 16.5303 5.6933L11.7574 0.920332C11.4645 0.627439 10.9896 0.627439 10.6967 0.920332C10.4038 1.21323 10.4038 1.6881 10.6967 1.98099L14.9393 6.22363L10.6967 10.4663C10.4038 10.7592 10.4038 11.234 10.6967 11.5269C10.9896 11.8198 11.4645 11.8198 11.7574 11.5269L16.5303 6.75396ZM0 6.97363H16V5.47363H0V6.97363Z"
+           fill="currentColor"
+          />
+         </svg>
+        </NavLink>
+       </div>
+       <div className="cs-height_30 cs-height_lg_30" />
+      </div>
+     </div>
+    </div>
+    <div className="cs-height_70 cs-height_lg_40" />
+   </div>
+  </>
+ );
 }
