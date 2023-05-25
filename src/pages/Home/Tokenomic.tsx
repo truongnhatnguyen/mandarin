b126ad93bcfc4adda1cd65f3edfc65cd09e3e441
PhuongHieu <fuhi.dev@gmail.com>
2023-05-25 09:01:09 +0700

    toeknomic mobile

--- src/pages/Home/Tokenomic.tsx
+++ src/pages/Home/Tokenomic.tsx
@@ -3,7 +3,10 @@ export function Tokenomic() {
   <div className="container-auto">
    <h2 className="cs-section_heading cs-style1 text-center">Tokenomic</h2>
    <div className="cs-height_45 cs-height_lg_45" />
-   <img src="/assets/home/tokenomic.png" alt="" />
+   <div className="flex justify-center">
+    <img className="hidden md:block" src="/assets/tokenomic/pc.png" alt="" />
+    <img className="md:hidden" src="/assets/tokenomic/mobile.png" alt="" />
+   </div>
   </div>
  );
 }
