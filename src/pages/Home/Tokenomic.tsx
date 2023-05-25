4fd1c60f3aa48590f885727a6b7c0f66765992f6
PhuongHieu <fuhi.dev@gmail.com>
2023-05-25 11:18:18 +0700

    Tokenmics

--- src/pages/Home/Tokenomic.tsx
+++ src/pages/Home/Tokenomic.tsx
@@ -1,7 +1,7 @@
 export function Tokenomic() {
  return (
   <div className="container-auto">
-   <h2 className="cs-section_heading cs-style1 text-center">Tokenomic</h2>
+   <h2 className="cs-section_heading cs-style1 text-center">Tokenomics</h2>
    <div className="cs-height_45 cs-height_lg_45" />
    <div className="flex justify-center">
     <img className="hidden md:block" src="/assets/tokenomic/pc.png" alt="" />
